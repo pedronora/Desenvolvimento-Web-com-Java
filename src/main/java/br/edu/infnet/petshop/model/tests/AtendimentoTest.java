04f2ce4fdc156d95c99f6fb0e827e5e1a0c4f12d
Pedro Nora <pedro@nora.vc>
2022-12-06 17:21:21 -0300

    Criados primeiros testes da classe Atendimento

--- src/main/java/br/edu/infnet/petshop/model/tests/AtendimentoTest.java
+++ src/main/java/br/edu/infnet/petshop/model/tests/AtendimentoTest.java
@@ -1,11 +1,25 @@
 package br.edu.infnet.petshop.model.tests;
 
 import br.edu.infnet.petshop.model.domain.Atendimento;
+import br.edu.infnet.petshop.model.domain.Pet;
 
 public class AtendimentoTest {
     public static void main(String[] args) {
         Atendimento a1 = new Atendimento();
+        a1.setDescricao("Primeiro Atendimento");;
+        a1.setEmergencia(true);;
+        a1.setPet(new Pet("Lua", "25/04/2022", "Cachorro", "SRD", 'F', false));
+       
         Atendimento a2 = new Atendimento();
+        a2.setDescricao("Segundo atendimento");
+        a2.setEmergencia(false);
+
         Atendimento a3 = new Atendimento();
+        a3.setDescricao("Terceiro atendimento");
+        a2.setEmergencia(false);
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PetShop - Cuidando do seu amigo!</title>
-</head>
-<body>
-    <p>Pedro Henrique Macedo Nora</p>
-</body>
-</html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>PetShop - Gestão</title>
+    </head>
+
+    <body>
+        <div class="container mt-3">
+            <h2>Pedro Nora</h2>
+        </div>
+    </body>
+
+    </html>
