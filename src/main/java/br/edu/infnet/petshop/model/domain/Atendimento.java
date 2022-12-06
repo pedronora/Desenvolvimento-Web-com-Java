9380a92bb41348e057ee6f0da1c0c3a3e8f21b26
Pedro Nora <pedro@nora.vc>
2022-12-06 17:21:01 -0300

    Criados atributos da classe Atendimento

--- src/main/java/br/edu/infnet/petshop/model/domain/Atendimento.java
+++ src/main/java/br/edu/infnet/petshop/model/domain/Atendimento.java
@@ -1,5 +1,62 @@
 package br.edu.infnet.petshop.model.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class Atendimento {
-    
+    private LocalDateTime data;
+    private String descricao;
+    private boolean emergencia;
+    private Pet pet;
+    private List<Servico> servicos;
+
+    public Atendimento() {
+        this.data = LocalDateTime.now();
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isEmergencia() {
+        return emergencia;
+    }
+
+    public void setEmergencia(boolean emergencia) {
+        this.emergencia = emergencia;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public List<Servico> getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(List<Servico> servicos) {
+        this.servicos = servicos;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return String.format("%s;%s;%s",
+                data.format(formatter),
+                descricao,
+                emergencia ? "Emergência" : "Normal");
+    }
 }
