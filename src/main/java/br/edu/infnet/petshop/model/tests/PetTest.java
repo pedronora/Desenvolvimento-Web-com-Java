5bbc1dee6c500db9fcb5f9c722ee2370f5d42ef4
Pedro Nora <pedro@nora.vc>
2022-12-06 16:43:27 -0300

    Criados primeiros testes da classe Pet

--- src/main/java/br/edu/infnet/petshop/model/tests/PetTest.java
+++ src/main/java/br/edu/infnet/petshop/model/tests/PetTest.java
@@ -4,8 +4,14 @@ import br.edu.infnet.petshop.model.domain.Pet;
 
 public class PetTest {
     public static void main(String[] args) {
-        Pet p1 = new Pet();
-        Pet p2 = new Pet();
-        Pet p3 = new Pet();
+        Pet p1 = new Pet("Lua", "25/04/2021", "Cachorro", "SRD", 'F', true);
+        Pet p2 = new Pet("Lola", "28/11/2020", "Cachorro", "SRD", 'F', false);
+        Pet p3 = new Pet("Teddy", "14/07/2002", "Cachorro", "Poodle", 'M', true);
+        Pet p4 = new Pet("Morcego", "10/09/2020", "Gato", "SRD", 'M', true);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
     }
 }
