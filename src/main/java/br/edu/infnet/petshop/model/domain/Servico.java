34131d8f6e6bb940ada6e4cfb56b4da03f95be5c
Pedro Nora <pedro@nora.vc>
2022-12-08 09:34:46 -0300

    Criada classe serviço

--- src/main/java/br/edu/infnet/petshop/model/domain/Servico.java
+++ src/main/java/br/edu/infnet/petshop/model/domain/Servico.java
@@ -1,5 +1,38 @@
 package br.edu.infnet.petshop.model.domain;
 
 public abstract class Servico {
-    
+    private String nome;
+    private float preco;
+    private int tempoMinutos;
+
+    public Servico(String nome, float preco, int tempoMinutos) {
+        this.nome = nome;
+        this.preco = preco;
+        this.tempoMinutos = tempoMinutos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public int getTempoMinutos() {
+        return tempoMinutos;
+    }
+
+    public abstract float calcularValorAtendimento();
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(nome);
+        sb.append(";");
+        sb.append(preco);
+        sb.append(";");
+        sb.append(tempoMinutos);
+        return sb.toString();
+    }
 }
