71988b0e927d74185ac8e5d7b886bf48d81edf2b
Pedro Nora <pedro@nora.vc>
2022-12-08 10:51:26 -0300

    Criados atributos da classe Consulta

--- src/main/java/br/edu/infnet/petshop/model/domain/Consulta.java
+++ src/main/java/br/edu/infnet/petshop/model/domain/Consulta.java
@@ -1,5 +1,58 @@
 package br.edu.infnet.petshop.model.domain;
 
 public class Consulta extends Servico {
-    
+    private boolean plantao;
+    private String descricao;
+    private String receita;
+
+    public Consulta(String nome, float preco, int tempoMinutos, boolean plantao, String descricao, String receita) {
+        super(nome, preco, tempoMinutos);
+        this.plantao = plantao;
+        this.descricao = descricao;
+        this.receita = receita;
+    }
+
+
+    public boolean isPlantao() {
+        return plantao;
+    }
+
+    public void setPlantao(boolean plantao) {
+        this.plantao = plantao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getReceita() {
+        return receita;
+    }
+
+    public void setReceita(String receita) {
+        this.receita = receita;
+    }
+
+    @Override
+    public float calcularValorAtendimento() {
+        return plantao ? (this.getPreco() + 100) : (this.getPreco() + 50);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(";");
+        sb.append(plantao ? "Plantão" : "Normal");
+        sb.append(";");
+        sb.append(descricao);
+        sb.append(";");
+        sb.append(receita);
+        sb.append(";");
+        sb.append("Valor total da Consulta: R$ " + this.calcularValorAtendimento());
+        return super.toString() + sb.toString();
+    }
 }
