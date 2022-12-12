aa5434646b410f6b7dae52fe41d70a1546e1a2ac
Pedro Nora <pedro@nora.vc>
2022-12-12 07:45:42 -0300

    Implementação dos testes da classe Banho

--- src/main/java/br/edu/infnet/petshop/model/tests/BanhoTest.java
+++ src/main/java/br/edu/infnet/petshop/model/tests/BanhoTest.java
@@ -8,7 +8,7 @@ import br.edu.infnet.petshop.model.exceptions.NoPriceException;
 public class BanhoTest {
     public static void main(String[] args) {
         try {
-            Banho b1 = new Banho("Banho", 0, 60, false, true, false);
+            Banho b1 = new Banho(null, 60, 60, false, true, false);
             System.out.println(b1);
 
         } catch (InvalidFieldException | NoPriceException | IllegalMinutesServiceException e) {
@@ -16,15 +16,31 @@ public class BanhoTest {
         }
 
         try {
-            Banho b2 = new Banho("Banho Premium", 55.9f, 90, true, true, true);
+            Banho b2 = new Banho("Banho", 0, 60, false, true, false);
             System.out.println(b2);
+
         } catch (InvalidFieldException | NoPriceException | IllegalMinutesServiceException e) {
             System.out.println("Erro: " + e.getMessage());
         }
 
         try {
-            Banho b3 = new Banho("Banho c/ Higiêne Bocal", 55.9f, 75, false, true, true);
+            Banho b3 = new Banho("Banho", 60, 10, false, true, false);
             System.out.println(b3);
+
+        } catch (InvalidFieldException | NoPriceException | IllegalMinutesServiceException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        try {
+            Banho b4 = new Banho("Banho Premium", 55.9f, 90, true, true, true);
+            System.out.println(b4);
+        } catch (InvalidFieldException | NoPriceException | IllegalMinutesServiceException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        try {
+            Banho b5 = new Banho("Banho c/ Higiêne Bocal", 55.9f, 75, false, true, true);
+            System.out.println(b5);
         } catch (InvalidFieldException | NoPriceException | IllegalMinutesServiceException e) {
             System.out.println("Erro: " + e.getMessage());
         }
